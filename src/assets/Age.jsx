410e74ea79cff160d21e80475191f212a6a70e8b
Luciano <lunick997@gmail.com>
2023-07-21 16:56:58 +0200

    commit

---
+++ src/assets/Age.jsx
@@ -0,0 +1,9 @@
+function Age(props){
+    if(props.age2 >=18){
+        return(
+            <p>your age is {props.age2}</p>
+        )
+    }
+    
+}
+export default Age
---
+++ src/assets/Welcome.jsx
@@ -0,0 +1,12 @@
+import Age from "./Age"
+function Welcome(props) {
+  return (
+    <div>
+      <p>Welcome {props.name}</p>
+      <Age age2 = {props.age}/>
+    </div>
+  )
+}
+
+export default Welcome
+
